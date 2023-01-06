e5944f5263af5f6f075a933b16bb8cf8fc81e17e
Renan Damascena Pereira <renandamascena@gmail.com>
2023-01-06 16:49:21 -0300

    gets e posts ainda não funcionam

--- src/app.js
+++ src/app.js
@@ -2,8 +2,9 @@ import express from "express"
 import cors from "cors"
 
 const app = express()
-app.use(cors())
 app.use(express.json())
+app.use(cors())
+
 
 const PORT = 5000
 
@@ -13,9 +14,51 @@ const users = [
 ]
 
 
-app.get("/",(req,res)=>{
+const tweets  = [
+    {
+        username: "bobesponja",
+        tweet: "eu amo o hub"
+    }
+]
+
+
+
+app.get("/sing-up",(req,res)=>{
     res.send(users)
 })
+
+
+
+
+app.post("/sing-up",(req,res)=>{
+    const user = req.body
+    users.push(user)
+    res.send("ok")
+})
+
+
+app.post("/tweets",(req,res)=>{
+   const newTweet = req.body
+   tweets.push(newTweet)
+   res.send(newTweet)
+})
+
+
+app.get("/tweets",(req,res)=>{
+
+    if(tweets.length <= 10){
+        res.send(tweets)
+    }
+
+    let lastTweets = []
+    
+    for(i = tweets.length - 1; i > tweets.length - 11; i--){
+        lastTweets.push(tweets[i])
+    }
+    res.send(lastTweets)
+})
+
+
 
 
 app.listen(PORT, ()=>{
